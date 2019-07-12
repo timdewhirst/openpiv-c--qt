284dc85b384741d4cb635e88d32d298c563962bd
timdewhirst <tim@bugless.co.uk>
2019-07-12 08:36:07 +0100

    experimantal work to have "small" images not be dynamically allocated

--- lib/include/openpiv/core/image.h
+++ lib/include/openpiv/core/image.h
@@ -2,6 +2,7 @@
 #pragma once
 
 // std
+#include <array>
 #include <cstdint>
 #include <exception>
 #include <iostream>
@@ -33,43 +34,36 @@ public:
     using index_t = size_t;
     using data_t = typename std::vector<T>;
 
-    using iterator = typename data_t::iterator;
-    using const_iterator = typename data_t::const_iterator;
-    using reverse_iterator = typename data_t::reverse_iterator;
-    using const_reverse_iterator = typename data_t::const_reverse_iterator;
+    using iterator = std::add_pointer_t<T>;
+    using const_iterator = std::add_pointer_t<std::add_const_t<T>>;
+    // using reverse_iterator = std::add_pointer_t<T>;
+    // using const_reverse_iterator = std::add_pointer_t<std::add_const_t<T>>;
 
     // ctor
     image()
-        : width_( 0 )
-        , height_( 0 )
-        {}
+        : image( 0, 0 )
+    {}
 
-    image( const image& ) = default;
-    image( image&& ) = default;
+    image( const image& rhs ) { operator=(rhs); }
+    image( image&& rhs ) { operator=(std::move(rhs)); }
 
     /// empty image
     image( uint32_t w, uint32_t h )
-        : width_( w )
-        , height_( h )
-        , data_( w*h )
+        : image( core::size{w, h} )
     {}
     image( const core::size& s )
-        : width_( s.width() )
-        , height_( s.height() )
-        , data_( s.area() )
-    {}
+    {
+        resize( s );
+    }
 
     /// image with default value
     image( uint32_t w, uint32_t h, T value )
-        : width_( w )
-        , height_( h )
-        , data_( w*h, value )
+        : image( {w, h}, value )
     {}
     image( const core::size& s, T value )
-        : width_( s.width() )
-        , height_( s.height() )
-        , data_( s.area(), value )
-    {}
+    {
+        resize( s, value );
+    }
 
     /// conversion from another similar image; expensive!
     template < template<typename> class ImageT,
@@ -79,55 +73,60 @@ public:
                    is_imagetype_v< ImageT<ContainedT> > >
                >
     explicit image( const ImageT<ContainedT>& p )
-        : width_( p.width() )
-        , height_( p.height() )
-        , data_( p.pixel_count() )
     {
-        *this = p;
+        operator=(p);
     }
 
     template <typename E,
               typename = typename std::enable_if_t< is_imageexpression_v<E> > >
     explicit image( const E& e )
     {
-        *this = e;
+        operator=(e);
     }
 
     /// resize the image; this is destructive and any data contained
     /// after a re-size should be considered invalid
-    void resize( uint32_t w, uint32_t h )
+    inline void resize( uint32_t w, uint32_t h ) { resize({w,h}); }
+    inline void resize( uint32_t w, uint32_t h, T value ) { resize({w,h}, value); }
+
+    /// resize the image; this is destructive and any data contained
+    /// after a re-size should be considered invalid
+    inline void resize( const core::size& s )
     {
-        if ( w == width_ && h == height_ )
+        if ( s == size() )
             return;
 
-        width_ = w;
-        height_ = h;
-        data_.resize( pixel_count() );
+        width_ = s.width();
+        height_ = s.height();
+        associate_data();
     }
-
-    /// resize the image; this is destructive and any data contained
-    /// after a re-size should be considered invalid
-    void resize( const core::size& s )
+    inline void resize( const core::size& s, T value )
     {
-        resize( s.width(), s.height() );
-    }
+        if ( s == size() )
+            return;
 
+        width_ = s.width();
+        height_ = s.height();
+        associate_data(value);
+    }
 
     /// assignment
-    image& operator=(const image& rhs)
+    inline image& operator=(const image& rhs)
     {
-        width_ = rhs.width();
-        height_ = rhs.height();
-        data_ = rhs.data_;
+        resize( rhs.size() );
+        typed_memcpy( data(), rhs.data(), rhs.pixel_count() );
+
         return *this;
     }
 
     /// move assignment
-    image& operator=(image&& rhs)
+    inline image& operator=(image&& rhs)
     {
-        data_   = std::move(rhs.data_);
-        width_  = std::move(rhs.width_);
-        height_ = std::move(rhs.height_);
+        small_data_   = std::move(rhs.small_data_);
+        large_data_   = std::move(rhs.large_data_);
+        width_        = std::move(rhs.width_);
+        height_       = std::move(rhs.height_);
+        associate_data();
 
         return *this;
     }
@@ -139,24 +138,22 @@ public:
                    pixeltype_is_convertible_v< ContainedT, T > &&
                    is_imagetype_v< ImageT<ContainedT> > >
                >
-    image& operator=( const ImageT<ContainedT>& p )
+    inline image& operator=( const ImageT<ContainedT>& p )
     {
         resize( p.size() );
-
-        // no alternative but to iterate
         for ( index_t i=0; i<p.pixel_count(); ++i )
-            convert( p[i], data_[i] );
+            convert( p[i], data()[i] );
 
         return *this;
     }
 
     template <typename E,
               typename = typename std::enable_if_t< is_imageexpression_v<E> > >
-    image& operator=(const E& e)
+    inline image& operator=(const E& e)
     {
         resize( e.size() );
         for ( index_t i=0; i<pixel_count(); ++i )
-            data_[i] = e[i];
+            data()[i] = e[i];
 
         return *this;
     }
@@ -167,23 +164,23 @@ public:
         return
             width_ == rhs.width_ &&
             height_ == rhs.height_ &&
-            data_ == rhs.data_;
+            typed_memcmp( data(), rhs.data(), pixel_count() ) == 0;
     }
     inline bool operator!=(const image& rhs) const { return !operator==(rhs); }
 
     /// pixel accessor
-    inline T& operator[](size_t i) { return data_[i]; }
+    inline T& operator[](size_t i) { return data()[i]; }
     inline const T& operator[](size_t i) const { return const_cast<image*>(this)->operator[](i); }
 
     /// pixel accessor by point
-    inline T& operator[]( const point2<uint32_t>& xy ) { return data_[xy[1]*width_ + xy[0]]; }
+    inline T& operator[]( const point2<uint32_t>& xy ) { return data()[xy[1]*width_ + xy[0]]; }
     inline const T& operator[]( const point2<uint32_t>& xy ) const
     {
         return const_cast<image*>(this)->operator[](xy);
     }
 
     /// raw data accessor
-    inline T* data() { return &data_[0]; }
+    inline T* data() { return (pixel_count() > small_data_.size()) ? &large_data_[0] : &small_data_[0]; }
     inline const T* data() const { return const_cast<image*>(this)->data(); }
 
     /// raw data by line
@@ -192,39 +189,57 @@ public:
         if (i>height_)
             exception_builder<std::range_error>() << "line out of range (" << i << ", max is: " << height_ << ")";
 
-        return &data_[i*width_];
+        return &data()[i*width_];
     }
     inline const T* line( size_t i ) const { return const_cast<image*>(this)->line(i); }
 
     /// iterators
-    iterator begin() { return std::begin( data_ ); }
-    iterator end() { return std::end( data_ ); }
-    const_iterator begin() const { return std::begin( data_ ); }
-    const_iterator end() const { return std::end( data_ ); }
-    reverse_iterator rbegin() { return std::rbegin( data_ ); }
-    reverse_iterator rend() { return std::rend( data_ ); }
-    const_reverse_iterator rbegin() const { return std::rbegin( data_ ); }
-    const_reverse_iterator rend() const { return std::rend( data_ ); }
+    inline iterator begin() { return data(); }
+    inline iterator end() { return data() + pixel_count(); }
+    inline const_iterator begin() const { return data(); }
+    inline const_iterator end() const { return data() + pixel_count(); }
+    // reverse_iterator rbegin() { return std::rbegin( data_ ); }
+    // reverse_iterator rend() { return std::rend( data_ ); }
+    // const_reverse_iterator rbegin() const { return std::rbegin( data_ ); }
+    // const_reverse_iterator rend() const { return std::rend( data_ ); }
 
     /// geometry accessors
-    inline const uint32_t width() const { return width_; }
-    inline const uint32_t height() const { return height_; }
-    inline const core::size size() const { return { width_, height_ }; }
-    inline const index_t pixel_count() const { return width_ * height_; }
+    inline uint32_t width() const { return width_; }
+    inline uint32_t height() const { return height_; }
+    inline core::size size() const { return { width_, height_ }; }
+    inline index_t pixel_count() const { return width_ * height_; }
     inline core::rect rect() const { return core::rect{ {}, { width_, height_ } }; }
 
     /// swap
-    void swap( image& rhs )
+    inline void swap( image& rhs )
     {
         std::swap( width_, rhs.width_ );
         std::swap( height_, rhs.height_ );
-        std::swap( data_, rhs.data_ );
+        std::swap( small_data_, rhs.small_data_ );
+        std::swap( large_data_, rhs.large_data_ );
+        associate_data();
     }
 
 private:
     uint32_t width_ = {};
     uint32_t height_ = {};
-    data_t data_;
+
+    inline void associate_data()
+    {
+        if ( pixel_count() > small_data_.size() )
+        {
+            large_data_.resize( pixel_count() );
+        }
+    }
+
+    inline void associate_data(T value)
+    {
+        associate_data();
+        typed_memset( data(), value, pixel_count() );
+    }
+
+    std::array<T, 32*32> small_data_;
+    data_t large_data_;
 };
 
 
